afe4073b7443cc7c65d3303bd25b5e458c318a1c
Nikita <silvernikchuprov@gmail.com>
2023-01-14 21:02:19 +0300

    работа идет

--- Task4.py
+++ Task4.py
@@ -1,37 +1,52 @@
-with open('Task 5 coded.txt', 'r') as data:
-    my_text = data.read()
+# with open('Task 5 coded.txt', 'r') as data:
+#     my_text = data.read()
 
-def encode_rle(ss):
-    str_code = ''
-    prev_char = ''
-    count = 1
-    for char in ss:
-        if char != prev_char:
-            if prev_char:
-                str_code += str(count) + prev_char
-            count = 1
-            prev_char = char
-        else:
-            count += 1
-    return str_code
+# def encode_rle(ss):
+#     str_code = ''
+#     prev_char = ''
+#     count = 1
+#     for char in ss:
+#         if char != prev_char:
+#             if prev_char:
+#                 str_code += str(count) + prev_char
+#             count = 1
+#             prev_char = char
+#         else:
+#             count += 1
+#     return str_code
 
             
-str_code = encode_rle(my_text)
-print(str_code)
+# str_code = encode_rle(my_text)
+# print(str_code)
 
-with open('Task5 encoded.txt', 'r') as data:
-    my_text2 = data.read()
+# with open('Task5 encoded.txt', 'r') as data:
+#     my_text2 = data.read()
 
-def decoding_rle(ss:str):
-    count = ''
-    str_decode = ''
-    for char in ss:
-        if char.isdigit():
-            count += char 
-        else:
-            str_decode += char * int(count)
-            count = ''
-    return str_decode
+# def decoding_rle(ss:str):
+#     count = ''
+#     str_decode = ''
+#     for char in ss:
+#         if char.isdigit():
+#             count += char 
+#         else:
+#             str_decode += char * int(count)
+#             count = ''
+#     return str_decode
 
-str_decode = decoding_rle(my_text2)
-print(str_decode)
+# str_decode = decoding_rle(my_text2)
+# print(str_decode)
+
+def encode(s):
+    encoding = ""
+    i = 0
+    while i < len(s):
+        count = 1
+        while i +1 < len(s) and s[i] == s[i+1]:
+            count = count + 1
+            i = i + 1
+        encoding += str(count) + s[i]
+        i = i + 1
+    return encoding
+if __name__ == '__main__':
+    s = str(input('Введите буквы без пробела: '))
+    print (encode(s))
